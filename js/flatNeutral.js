0402cd7b6cd81ff508d3ec44081f8b001f430d6d
Jacob Menick <jmenick@reed.edu>
2014-10-14 19:30:53 -0700

    finished price EQ Calculations.

--- js/flatNeutral.js
+++ js/flatNeutral.js
@@ -3,6 +3,24 @@
 
 // utility
 
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+ 
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+// print array of arrays. 
+function paoa(matrix) {
+		for (var i = 0; i < matrix.length; i++) {
+				console.log(matrix[i]);
+		}
+}
+
 // random integer in range [low, high], inclusive.
 function randomInt(low, high) {
     return Math.round(Math.random()*(high - low)) + low;
@@ -58,6 +76,44 @@ function filterGenLessThan(population, genLimit) {
 		return filtered;
 }
 
+function filterGenInRange(population, gEq, lessThan) {
+		var filtered = [];
+		for (var i = 0; i < population.length; i++) {
+				if (population[i].generation >= gEq & population[i].generation < lessThan) {
+						filtered.push(population[i]);
+				}
+		}
+		return filtered;
+}
+
+function hasTrait(entity, trait) {
+		if (contains(entity.traits, trait)) {
+				return 1;
+		} else {
+				return 0;
+		}
+}
+
+function getIdentifier(entity) {
+		return [entity.generation, entity.genIndex];
+}
+
+function isParentOf(entity, parent) {
+		for (var i = 0; i < entity.parents.length; i++) {
+				var ident = getIdentifier(entity.parents[i]);
+				if (arraysEqual(ident,getIdentifier(parent))) return 1;
+		}
+		return 0;
+}
+
+function isChildOf(entity, child) {
+		for (var i = 0; i < entity.children.length; i++) {
+				var ident = getIdentifier(entity.children[i]);
+				if (arraysEqual(ident, getIdentifier(child))) return 1;
+		}
+		return 0;
+}
+
 function sumVec(xs) {
     return xs.reduce(function(a, b) {return a+b;});
 }
@@ -65,6 +121,10 @@ function sumVec(xs) {
 function normalize(vector) {
     var sum = sumVec(vector);
     return vector.map(function(x) {return x/sum;});
+}
+
+function scaleBy(vector, scalar) {
+		return vector.map(function(x) { return x*scalar;});
 }
 
 function fillZeroes(size) {
@@ -290,3 +350,146 @@ function getTraitCSV(nm) {
 }
 
 // Price Equation Calculations. 
+
+function avg(X) {
+		return sumVec(X)/(X.length);
+}
+
+function getColumn(matrix, column) {
+		var col = [];
+		var numRows = matrix.length;
+		for (var i = 0; i < numRows; i++) {
+				col.push(matrix[i][column])
+		}
+		return col;
+}
+
+function transpose(matrix) {
+		var newMat = [];
+		var numRows = matrix.length;
+		var numCols = matrix[0].length;
+		for (var i = 0; i < numCols; i++) {
+				newMat.push(getColumn(matrix, i));
+		}
+		return newMat;
+}
+
+
+function numChildren(matrix, row) {
+		return sumVec(matrix[row]);
+}
+
+function numParents(matrix, column) {
+		var col = getColumn(matrix, column);
+		return sumVec(col);
+}
+
+function cov(X, Y) {
+		if (X.length != Y.length) throw "Exception: must have same length.";
+		var n = X.length;
+		var xMean = avg(X);
+		var yMean = avg(Y);
+		var runTot = 0;
+		for (var i = 0; i < n; i++) {
+				runTot += (X[i] - xMean)*(Y[i] - yMean);
+		}
+		return runTot/(n-1);
+}
+
+function getC(ancestralPop, descendantPop) {
+		var matrix = [];
+		for (var i = 0; i < ancestralPop.length; i++) {
+				var childArray = fillZeroes(descendantPop.length);
+				for (var j = 0; j < descendantPop.length; j++) {
+						childArray[j] = isParentOf(descendantPop[j], ancestralPop[i]);
+				}
+				matrix.push(childArray);
+		}
+		return matrix;
+}
+
+function traitChar(pop, trait) {
+		return pop.map(function(x) { return hasTrait(x, trait); });
+}
+
+function numChildrenChar(matrix) {
+		return matrix.map(function(x) {return sumVec(x);});
+}
+
+function numChildrenRelChar(matrix) {
+		var avgNumC = avgNumChildren(matrix);
+		return scaleBy(numChildrenChar(matrix), 1/avgNumC);
+}
+
+function numParentsChar(matrix) {
+		var mt = transpose(matrix);
+		return mt.map(function(x) { return sumVec(x); })
+}
+
+function numParentsRelChar(matrix) {
+		var avgNumP = avgNumParents(matrix);
+		return scaleBy(numParentsChar(matrix), 1/avgNumP);
+}
+
+function avgNumChildren(matrix) {
+		return avg(numChildrenChar(matrix));
+}
+
+function avgNumParents(matrix) {
+		return avg(numParentsChar(matrix));
+}
+
+function traitDiff(parent, child, trait) {
+		return hasTrait(parent, trait) - hasTrait(child, trait);
+}
+
+function numTotalCites(matrix) {
+		return sumVec(numChildrenChar(matrix));
+}
+
+function mutation(parent, child, trait) {
+		if (isChildOf(parent, child) == 1) {
+				return traitDiff(parent, child, trait);
+		} 
+		return 0;
+}
+
+function diffFecundity(aPop, dPop, trait) {
+		var c = getC(aPop, dPop);
+		var aTraitChar = traitChar(aPop, trait);
+		var aChildrenChar = numChildrenRelChar(c);
+		return cov(aTraitChar, aChildrenChar);
+}
+
+function diffMutation(aPop, dPop, trait) {
+		var c = getC(aPop, dPop);
+		var mutTot = 0;
+		for (var i = 0; i < aPop.length; i++) {
+				for (var j = 0; j < dPop.length; j++) {
+						mutTot += mutation(aPop[i], dPop[j], trait);
+				}
+		}
+		return mutTot / numTotalCites(c);
+}
+
+function diffConvergence(aPop, dPop, trait) {
+		var c = getC(aPop, dPop);
+		var dTraitChar = traitChar(dPop, trait);
+		var dParentsChar = numParentsRelChar(c);
+		return cov(dTraitChar, dParentsChar);
+}
+
+function getPriceEQs(population, startGen, endGen, trait) {
+
+		var A = filterGenInRange(population, 0, startGen);
+		console.log("Ancestral Population: gens 0 through " + (startGen - 1) + ". (" +A.length + " entities)" );
+		var D = filterGenInRange(population, startGen, endGen);
+		console.log("Descendant Population: gens " + (startGen - 1) + " to " + (endGen - 1) + ".(" + D.length + "entities)");
+		console.log("Differential Fecundity for trait 1: " + diffFecundity(A, D, 1) + ".");
+		console.log("Differential Mutation for trait 1: " + diffMutation(A, D, 1) + ".");
+		console.log("Differential Convergence for trait 1: " + diffConvergence(A, D, 1) + ".");
+}
+
+
+
+
